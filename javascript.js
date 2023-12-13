8717d0c6124e39e5641978e58b06830f2bf9ec67
obsessed <spardax345@gmail.com>
2023-12-13 20:26:06 -0300

    I made the calculator work correctly in a different way, now to add clear and backspace button

--- javascript.js
+++ javascript.js
@@ -1,52 +1,23 @@
-  const add = function (array) {
-    return array.reduce((result, current) => result + current)
-  };
-  
-  const subtract = function (array) {
-    return array.reduce((result, current) => result - current)
-  };
+function calculate(firstNumber, operator, secondNumber) {
 
-  const multiply = function (array) {
-    return array.reduce((product, current) => product * current)
-  };
+	firstNumber = Number(firstNumber);
+	secondNumber = Number(secondNumber);
 
-  const divide = function(array){
-    return array.reduce((quotient, current) => quotient / current)
-  };
-
-let num1= 0;
-let num2= 0;
-let operator;
-
-function operate(num1,operator,num2){
-
-num1 = prompt('enter a number');
-num2 = prompt('enter another number');
-operator = prompt('enter an operator');
-let result;
-
-if(operator == "+"){
-   result = add(+num1,+num2);
-}else if(operator == "x"){
-  result =  multiply([+num1,+num2]);
-}else if(operator == "/"){
-   result = divide([+num1,+num2]);
-}else if(operator == '-'){
-   result = subtract(+num1,+num2);
+    if (operator === 'plus' || operator === '+') return firstNumber + secondNumber;
+    if (operator === 'minus' || operator === '-') return firstNumber - secondNumber;
+    if (operator === 'multiply' || operator === 'x') return firstNumber * secondNumber;
+    if (operator === 'divide' || operator === '/') return firstNumber / secondNumber;
+    
 }
 
-alert(result);
-}
+
 
 const numButtons = document.querySelectorAll('.num');
 const equalButton = document.querySelector('.equal');
-const minusButton =document.querySelector('.minus');
-const multiplyButton = document.querySelector('.multiply');
-const divideButton = document.querySelector('.divide');
-const addButton = document.querySelector('.plus');
+const operationButton = document.querySelectorAll('.operation');
 let display = document.querySelector('.result');
-let result = 0;
-let numbers = [];
+let secondDisplay = document.querySelector('.storedValue');
+let result = [];
 
 numButtons.forEach(button => {
     button.addEventListener('click', function() {
@@ -55,34 +26,47 @@ numButtons.forEach(button => {
     });
 });
 
-multiplyButton.addEventListener('click', function(){
 
-    let newNumber = display.value;
-    numbers.push(newNumber);
-    display.value ='';
-    console.log(multiply(numbers));
+operationButton.forEach(button => {
+  button.addEventListener('click', function() {
+      const buttonValue = this.textContent; 
+      const previousValue = display.value;
+      result.push(previousValue);
+      secondDisplay.value = secondDisplay.value + display.value + buttonValue;
+      display.value = '';
+  });
 });
+equalButton.addEventListener('click', function(){
 
-divideButton.addEventListener('click', function(){
-
-    let newNumber = display.value;
-    numbers.push(newNumber);
-    display.value ='';
-    console.log(divide(numbers));
-});
-
-addButton.addEventListener('click', function(){
-
-    let newNumber = display.value;
-    numbers.push(newNumber);
-    display.value ='';
-    console.log(add(numbers));
-});
-
-minusButton.addEventListener('click', function(){
-
-    let newNumber = display.value;
-    numbers.push(newNumber);
-    display.value ='';
-    console.log(subtract(numbers));
+  const previousValue = display.value;
+  result.push(previousValue);
+  secondDisplay.value = secondDisplay.value + display.value;
+  if(secondDisplay.value.includes("+")){
+    for (let i = result.length - 2; i < result.length; i++) {
+      
+      display.value = +result[i] + +result[i-1]
+  }
+  secondDisplay.value ='';
+  }
+  if(secondDisplay.value.includes("x")){
+    for (let i = result.length - 2; i < result.length; i++) {
+      
+      display.value = +result[i] * +result[i-1]
+      secondDisplay.value ='';
+  }
+  }
+  if(secondDisplay.value.includes("-")){
+    for (let i = result.length - 2; i < result.length; i++) {
+      
+      display.value = +result[i-1] - +result[i]
+      secondDisplay.value ='';
+  }
+  }
+  if(secondDisplay.value.includes("/")){
+    for (let i = result.length - 2; i < result.length; i++) {
+      
+      display.value = +result[i-1] / +result[i]
+      secondDisplay.value ='';
+  }
+  }
 });
