191e3c3de7b40738a224b949073a4a93035a261d
obsessed <spardax345@gmail.com>
2023-12-14 19:18:56 -0300

    can manage multiple operations at a time

--- javascript.js
+++ javascript.js
@@ -1,14 +1,41 @@
-function calculate(firstNumber, operator, secondNumber) {
-
-	firstNumber = Number(firstNumber);
-	secondNumber = Number(secondNumber);
-
-    if (operator === 'plus' || operator === '+') return firstNumber + secondNumber;
-    if (operator === 'minus' || operator === '-') return firstNumber - secondNumber;
-    if (operator === 'multiply' || operator === 'x') return firstNumber * secondNumber;
-    if (operator === 'divide' || operator === '/') return firstNumber / secondNumber;
-    
+function add(a, b) {
+  return a + b;
 }
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+      return "Error: Division by zero";
+  }
+  return a / b;
+}
+
+function solveOperation(operation,a, b){
+
+if(operation =='+'){
+
+  return add(a,b);
+}else if(operation == '-'){
+
+  return subtract(a,b);
+}else if(operation == '/'){
+
+  return divide(a,b);
+}else if(operation =='x'){
+  return multiply(a,b);
+}else{
+  return 'invalid operation';
+}
+
+}
+
 
 
 
@@ -19,6 +46,8 @@ const clearButton = document.querySelector('.clear');
 let display = document.querySelector('.result');
 let secondDisplay = document.querySelector('.storedValue');
 let result = [];
+let previousValue = 0;
+let previousOperation = null;
 
 numButtons.forEach(button => {
     button.addEventListener('click', function() {
@@ -36,48 +65,24 @@ clearButton.addEventListener('click', function(){
 
 operationButton.forEach(button => {
   button.addEventListener('click', function() {
-      const buttonValue = this.textContent; 
-      const previousValue = display.value;
-      result.push(previousValue);
-      secondDisplay.value = secondDisplay.value + display.value + buttonValue;
-      display.value = '';
-  });
+      const currentOperation = this.textContent.trim();
+      if (previousOperation !== null) {
+        previousValue = solveOperation(previousOperation, previousValue, +display.value);
+        display.value = previousValue;
+    } else {
+        previousValue = +display.value;
+    }
+
+    display.value = "";
+    secondDisplay.value = previousValue;
+    previousOperation = currentOperation;
+    
 });
+  });
+
+
 equalButton.addEventListener('click', function(){
 
-  const previousValue = display.value;
-  result.push(previousValue);
-  secondDisplay.value = secondDisplay.value + display.value;
-  if(secondDisplay.value.includes("+")){
-    for (let i = result.length - 2; i < result.length; i++) {
-      
-      const unroundedAnswer = +result[i] + +result[i-1];
-      display.value = Math.round(unroundedAnswer * 100) / 100;
-  }
-  secondDisplay.value ='';
-  }
-  if(secondDisplay.value.includes("x")){
-    for (let i = result.length - 2; i < result.length; i++) {
-      
-      const unroundedAnswer = +result[i] * +result[i-1];
-      display.value = Math.round(unroundedAnswer * 100) / 100;
-      secondDisplay.value ='';
-  }
-  }
-  if(secondDisplay.value.includes("-")){
-    for (let i = result.length - 2; i < result.length; i++) {
-      
-      const unroundedAnswer = +result[i-1] - +result[i];
-      display.value = Math.round(unroundedAnswer * 100) / 100;
-      secondDisplay.value ='';
-  }
-  }
-  if(secondDisplay.value.includes("/")){
-    for (let i = result.length - 2; i < result.length; i++) {
-      
-      const unroundedAnswer = +result[i-1] / +result[i];
-      display.value = Math.round(unroundedAnswer * 100) / 100;
-      secondDisplay.value ='';
-  }
-  }
+  
+  secondDisplay.value = solveOperation(previousOperation, previousValue, +display.value);
 });
